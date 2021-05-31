facca87c8146f12e679501038d078efc707cec11
oebitw <engomarewies@gmail.com>
2021-05-31 16:25:08 +0300

    index.html

--- public/src/app.js
+++ public/src/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+let bgColor= 'black';
+let snakeColor='red';
+let foodColor='green';
+const gameScreen = document.getElementById('gameScreen');
+let canvas , ctx;
+
+
+function init(){
+
+  canvas = document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
+  
+  canvas.width = canvas.height = 600;
+  
+  ctx.fillStyle = bgColor;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  
+  document.addEventListener('keydown', keydown);
+}
+function keydown(e){
+  console.log(e.keyCode);
+}
+
+init();
+
