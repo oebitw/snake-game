68b68a02e5f11fa41eec980fd3c5419e698d9d88
yasmeenokh <ykhatib44@gmail.com>
2021-06-06 22:12:31 +0300

    added the msg queue functionalty

--- public/app.js
+++ public/app.js
@@ -10,24 +10,35 @@ const username = prompt('What is your name?');
 appendMessage('You joined');
 socket.emit('newUser', username);
 
+socket.on('oldChat', data=>{
+  if(data.messageStoreArray){
+    console.log('DATA',data)
+  data.messageStoreArray.forEach(element=>{
+    appendMessage(` ${element.username} : ${element.message}`);
+  })}
+})
 socket.on('chatMsg', data=>{
+  console.log('DATAAAAAAAA:', data)
   appendMessage(` ${data.username} : ${data.message}`);
 });
 
+
 socket.on('userConnected', username=>{
   appendMessage(`${username} is connected`);
+
 });
-// socket.emit('chatMsg', 'hello');
 socket.on('userDisconnet', username=>{
+  console.log('user disconnect function', username)
   appendMessage(`${username} is Disconnected`);
 });
 messageForm.addEventListener('submit', e=>{
   e.preventDefault();
   const message = messageInput.value;
   appendMessage(`You : ${message}`);
-
+  
   socket.emit('sendChatMsg', message);
   messageInput.value = '';
+  console.log('Event Listener:', message)
 });
 
 function appendMessage(message){
@@ -35,3 +46,4 @@ function appendMessage(message){
   messageElement.innerText = message;
   messageContainer.append(messageElement);
 }
+
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 'use strict'; 
 
-// const io = require('socket.io')(3030);
 const app = require('express')();
 const http = require('http');
 const httpServer=http.createServer(app);
@@ -16,21 +15,30 @@ app.use(cors());
 app.use(express.static('./public'));
 
 const users = {};
-
+let messageStore = {
+  messageStoreArray: []
+};
+let messageStoreArray = [];
+console.log('message Store :::',messageStore)
 io.on('connection', socket =>{
   socket.on('newUser', username=>{
     users[socket.id]= username;
-    socket.broadcast.emit('userConnected', username);
+    socket.emit('oldChat', messageStore)
+        socket.broadcast.emit('userConnected', username);
   });
-  
   socket.on('sendChatMsg', message=>{
+    console.log('disconnect', io.sockets.adapter.rooms.get(socket.id))
+    messageStoreArray.push({message: message, username: users[socket.id]})
+    messageStore.messageStoreArray= messageStoreArray ;
     socket.broadcast.emit('chatMsg', {message: message, username: users[socket.id]});
   });
-  socket.on('disconnet', ()=>{
+  socket.on('disconnect', () => {
     socket.broadcast.emit('userDisconnet', users[socket.id]);
+    if (!io.sockets.adapter.rooms.get(socket.id)) {
+      delete messageStore.messageStoreArray;
+    }
     delete users[socket.id];
-  });
-  
+})
 });
 
 
